e91788e662199e3c0d2bac95d91c029a446784ee
Robin Panta <hacktivistic@gmail.com>
2022-07-05 16:18:55 +0530

    add soft deletes plugin

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ class MongooseServiceProvider extends ServiceProvider {
         this.app.singleton('Haluka/Provider/Mongoose', function (app, { MongooseConfig }) {
             return new MongooseManager(MongooseConfig, app)
         })
+
+        this.app.singleton('Haluka/Provider/Mongoose/ModelBinding', function (app, { MongooseConfig }) {
+            return new MongooseManager(MongooseConfig, app)
+        })
     }
 }
 
@@ -25,6 +29,8 @@ class MongooseManager {
         return await mongoose.createConnection(conf.connString, conf.options).asPromise()
     }
     async setupAll() {
+        mongoose.plugin(this._softDeletesPlugin())
+        
         for (var conf in this.config.connections) {
             var connection = this.config.connections[conf]
             this.connections[conf] = await this.mongoConnect(connection)
@@ -58,5 +64,47 @@ class MongooseManager {
             await this.close(conn)
         }
     }
+    _softDeletesPlugin () {
+        return (schema) => {
+            schema.add({ softDeleted: false })
+        }
+    }
 }
 exports.default = MongooseServiceProvider
+
+class ModelBinding {
+
+    static withRoute (req, Model, includeSoftDeletes = false) {
+
+        validateBindingModel(Model)
+        validateRequest(req, Model.getRouteParamKey())
+
+        let query = {}
+        query[Model.getModelParamKey()] = req.params[Model.getRouteParamKey()]
+        
+        let item = undefined
+        if (Model.binding && typeof(Model.binding) == 'function')
+            item = Model.binding(req)
+        else
+            item = Model.find({ id: req.params[Model.getRouteParamKey()], softDeleted: false })
+
+    }
+
+    static withForm (req, Model)
+
+}
+
+function validateBindingModel (Model) {
+    if (!Model.getModelParamKey || typeof(Model.getModelParamKey) !== 'function')
+    throw 'Model Identifier not set. Please add a static getModelParamKey() function to your Model.'
+
+    if (!Model.getRouteParamKey || typeof(Model.getRouteParamKey) !== 'function')
+    throw 'Route Param Identifier not set. Please add a static getRouteParamKey() function to your Model.'
+}
+
+function validateRequest (req, key) {
+    if (!req) throw "Invalid Request passed for Model Binding."
+    if (!req.params[key]) throw "Route Params doesn't exist"
+}
+
+exports.ModelBinding = ModelBinding
